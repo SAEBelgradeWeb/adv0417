2468b2582027d4e4607eacf7a57dcef13d9c91aa
Vladimir <v.lelicanin@dooapp.net>
2017-05-08 20:45:26 +0200

    cas9

---
+++ cas9/js/main.js
@@ -0,0 +1,20 @@
+
+
+
+//next, prev, last, first, eq(1),
+//children - ide samo na decu, find - beskonacno u dubinu
+//parents, parent, colosest
+//siblings
+
+
+//css() - menja css
+//addClass dodaje klasu
+//removeClass skida klasu
+//attr - get i set atributa
+
+var prop = {
+  color: "red",
+  fontSize: "25px"
+};
+
+$('.start').css(prop);
